180e409c2417c71c2fb658aa55c7f92edd593206
JakobDexl <jakpokerface@web.de>
2018-06-05 13:22:36 +0200

    updated

--- vis_keras/__init__.py
+++ vis_keras/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 
 from . import vis_utils
 from . import vis_core
-from . vis_keras import Model_explorer
+from .vis_keras import Model_explorer
 
 __version__ = '1.0.0'
